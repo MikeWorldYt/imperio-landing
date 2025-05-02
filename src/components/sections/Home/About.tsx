80011b3cf8a06ab4b396713c75eaf880b43f831e
mikeworldyt <diazmig98@gmail.com>
2025-05-01 21:30:54 -0600

    style(about): enhance shadow effect for floating card

--- src/components/sections/Home/About.tsx
+++ src/components/sections/Home/About.tsx
@@ -28,7 +28,7 @@ const AboutImage = () => {
             <style>{`
                 .floating-card {
                     overflow: hidden;
-                    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+                    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.35);
                 }
 
                 .floating-card::before {

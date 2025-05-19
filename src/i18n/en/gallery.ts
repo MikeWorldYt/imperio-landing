ac5ad8b7195c85f9b6b38a94396c31aac9beeab2
mikeworldyt <diazmig98@gmail.com>
2025-05-19 10:00:53 -0600

    feat(gallery): update titles and add descriptions

--- src/i18n/en/gallery.ts
+++ src/i18n/en/gallery.ts
@@ -1,12 +1,11 @@
 import type { LangTypes } from "../utils/types";
 
 export const galleryTexts: LangTypes = {
-    title: 'Hello Gallery',
+    title: 'Our Recent Projects',
     subtitle: 'This is our project gallery.',
+    description: `Discover some of our latest work`,
 
     header: {
-        title: 'Hello Header',
-        subtitle: 'We are in the header page.',
         nav1: 'Home',
         nav2: 'Gallery',
         nav3: 'Services',
--- src/i18n/es/gallery.ts
+++ src/i18n/es/gallery.ts
@@ -1,12 +1,10 @@
 import type { LangTypes } from "../utils/types";
 
 export const galleryTexts: LangTypes = {
-    title: 'Hola Galería',
-    subtitle: 'Esta es nuestra galería de proyectos.',
+    title: 'Algunos de nuestros proyectos',
+    description: `Descubre algunos de nuestros proyectos recientes`,
 
     header: {
-        title: 'Hola Header',
-        subtitle: 'Estamos en la header page.',
         nav1: 'Home',
         nav2: 'Galería',
         nav3: 'Servicios',

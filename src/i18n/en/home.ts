efb07d36df8f8270c9495d8aea899216be4cccb6
mikeworldyt <diazmig98@gmail.com>
2025-05-31 13:37:25 -0600

    chore(i18n): update hero section texts

--- src/i18n/en/home.ts
+++ src/i18n/en/home.ts
@@ -14,7 +14,18 @@ export const homeTexts: LangTypes = {
     },
 
     hero: {
-        title: 'Hello Hero',
-        subtitle: 'We are in the hero page.'
+        title: 'We building, with precision and excellence.',
+        description: 'desc in english',
+        button_cta: 'Contact Us',
+        form_title: 'Get a Quote',
+        form_subtitle: 'Call us Today 909 4308 805',
+        form_content: {
+            name: 'Name',
+            email: 'Email',
+            phone: 'Phone',
+            message: 'Message',
+            contact_method: 'Preferred Contact Method',
+            submit_button: 'Submit'
+        }
     }
 };
--- src/i18n/es/home.ts
+++ src/i18n/es/home.ts
@@ -1,6 +1,6 @@
-import type { HomeTexts } from '../utils/types';
+import type { LangTypes } from '../utils/types';
 
-export const homeTexts: HomeTexts = {
+export const homeTexts: LangTypes = {
     title: 'Hola Home',
     subtitle: 'Estamos en la home page.',
 
@@ -15,7 +15,18 @@ export const homeTexts: HomeTexts = {
     },
 
     hero: {
-        title: 'Hola Hero',
-        subtitle: 'Estamos en la hero page.'
+        title: 'Construimos con precisión y excelencia.',
+        description: 'desc en español',
+        button_cta: 'Contáctanos',
+        form_title: 'Solicitar una Cotización',
+        form_subtitle: 'Llámanos hoy 909 4308 805',
+        form_content: {
+            name: 'Nombre',
+            email: 'Correo',
+            phone: 'Teléfono',
+            message: 'Mensaje',
+            contact_method: 'Metodo de contacto',
+            submit_button: 'Enviar'
+        }
     }
 };

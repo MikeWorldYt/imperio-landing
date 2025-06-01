c9515c8fdbd118a3937277e5da60c149adecb3b0
mikeworldyt <diazmig98@gmail.com>
2025-06-01 12:43:33 -0600

    feat(Services): add carousel functionality + navigation buttons

--- src/components/sections/Home/Services.tsx
+++ src/components/sections/Home/Services.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Card from '../../ui/Card';
 import { servicesAPI } from '../../../data/servicesData';
 import { homeTexts as enHomeTexts } from '../../../i18n/en/home';
@@ -29,20 +30,48 @@ const homeTextsByLang: Record<string, LangTypes> = {
 const Services = ({ lang }: ServicesProps) => {
     const texts = homeTextsByLang[lang].services;
     const isDesktop = useIsDesktop();
+    // Carrousel
+    const carouselRef = useRef<HTMLDivElement>(null);
+    const scroll = (dir: "left" | "right") => {
+      if (carouselRef.current) {
+        const width = carouselRef.current.clientWidth;
+        carouselRef.current.scrollBy({
+          left: dir === "left" ? -width : width,
+          behavior: "smooth",
+        });
+      }
+    };
 
     return (
       <section className="w-full py-16 bg-blue-600/50 flex flex-col items-center justify-center text-black">
         <h2 className="global-section mb-8">
           {texts.title}
         </h2>
-        <div className="w-full max-w-screen-xl px-4">
+        <div className="relative w-full max-w-screen-xl px-4">
           {isDesktop ? ( // Desktop view
-            <div className="flex overflow-x-auto snap-x snap-mandatory space-x-4 scrollbar-hide scroll-smooth px-1">
-              {servicesAPI.map((service, index) => (
-                <div className="snap-start shrink-0 w-[calc(100%/3-1rem)] h-full py-2">
-                  <Card key={index} {...service} />
+            <div className="flex overflow-x-auto snap-x snap-mandatory space-x-4 scrollbar-hide scroll-smooth px-1 ">
+              {/* Left button */}
+              <button
+                onClick={() => scroll("left")}
+                className="absolute left-4 top-1/2 -translate-y-1/2 z-10 bg-blue-100 rounded-full shadow-xl p-2"
+              >
+                <ChevronLeft className="text-blue-950" />
+              </button>
+              {/* CARROUSEL CONTAINER */ }
+                <div ref={carouselRef} className="flex overflow-x-auto snap-x snap-mandatory space-x-4 scrollbar-hide scroll-smooth">
+                  {servicesAPI.map((service, index) => (
+                    <div className="snap-start shrink-0 w-[calc(100%/3-1rem)] h-full py-2">
+                      <Card key={index} {...service} />
+                    </div>
+                  ))}
                 </div>
-              ))}
+              {/* Right button  */}
+              <button
+                onClick={() => scroll("right")}
+                className="absolute right-4 top-1/2 -translate-y-1/2 z-10 bg-blue-100 rounded-full shadow-xl p-2"
+              >
+                <ChevronRight className="text-blue-950" />
+              </button>
             </div>
           ) : ( // Mobile view
             <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 w-full max-w-screen-xl px-4">

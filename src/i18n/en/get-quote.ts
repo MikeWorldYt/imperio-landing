68adcc777d334c73afd97bb6d585ee316cb4588a
mikeworldyt <diazmig98@gmail.com>
2025-06-03 16:41:31 -0600

    feat(i18n): update quote titles and headers - translations

--- src/i18n/en/get-quote.ts
+++ src/i18n/en/get-quote.ts
@@ -1,4 +1,14 @@
 export const quoteTexts = {
-    title: 'Hello Quote',
+    // Head elements
+    title: 'Get a Quote - Imperio Pools',
+    // Header texts
+    header: {
+        title: 'Hello Header',
+        subtitle: 'We are in the header page.',
+        nav1: 'Home',
+        nav2: 'Gallery',
+        nav3: 'Services',
+        nav4: 'Get Quote'
+    },
     subtitle: 'This is our quote.'
 };

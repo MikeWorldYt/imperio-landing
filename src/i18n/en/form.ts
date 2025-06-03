0d5fb68700c1bd7cffc39db4d8552a8a3e6c5410
mikeworldyt <diazmig98@gmail.com>
2025-06-03 16:39:29 -0600

    feat(i18n): add additional services

--- src/i18n/en/form.ts
+++ src/i18n/en/form.ts
@@ -8,6 +8,13 @@ export const formTexts: LangTypes = {
     phone: 'Phone',
     service: 'Service',
     service_placeholder: 'Select a Service',
+    service_1: 'Pool Remodeling',
+    service_2: 'Replastering',
+    service_3: 'Copying, Tile',
+    service_4: 'Deck and BBQ Bars',
+    service_5: 'Decorative Features and Fountains',
+    service_6: 'New Pool Construction',
+    service_7: 'Pool Maintenance',
     message: 'Message',
     contact_method: 'Preferred Contact Method',
     submit_button: 'Submit'
--- src/i18n/es/form.ts
+++ src/i18n/es/form.ts
@@ -8,6 +8,13 @@ export const formTexts: LangTypes = {
     phone: 'Teléfono',
     service: 'Servicio',
     service_placeholder: 'Selecciona un Servicio',
+    service_1: 'Remodelación de Piscina',
+    service_2: 'Replaster',
+    service_3: 'Bordillos y Barldosas',
+    service_4: 'Terrazas y BBQ Barras',
+    service_5: 'Decoraticiones y Fuentes',
+    service_6: 'Construcción de Piscinas',
+    service_7: 'Mantenimiento de Piscinas',
     message: 'Mensaje',
     contact_method: 'Metodo de contacto',
     submit_button: 'Enviar'

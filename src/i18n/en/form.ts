6d08eb0f5aaaf0b95e6518b3fae783a8d98be16d
mikeworldyt <diazmig98@gmail.com>
2025-06-03 15:27:30 -0600

    feat(i18n): add form text localization

---
+++ src/i18n/en/form.ts
@@ -0,0 +1,13 @@
+import type { LangTypes } from "../utils/types";
+
+export const formTexts: LangTypes = {
+    title: 'Get a Quote',
+    subtitle: 'Call us Today 909 0000 000',
+    name: 'Name',
+    email: 'Email',
+    phone: 'Phone',
+    service: 'Service',
+    message: 'Message',
+    contact_method: 'Preferred Contact Method',
+    submit_button: 'Submit'
+}
---
+++ src/i18n/es/form.ts
@@ -0,0 +1,13 @@
+import type { LangTypes } from "../utils/types";
+
+export const formTexts: LangTypes = {
+    title: 'Solicitar una Cotización',
+    subtitle: 'Llámanos hoy 909 0000 000',
+    name: 'Nombre',
+    email: 'Correo',
+    phone: 'Teléfono',
+    service: 'Servicio',
+    message: 'Mensaje',
+    contact_method: 'Metodo de contacto',
+    submit_button: 'Enviar'
+}

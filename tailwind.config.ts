46df4d4d173a62560597684fcef5392704b07949
mikeworldyt <diazmig98@gmail.com>
2025-06-05 12:32:30 -0600

    feat(dev-fdntl): add custom text shadow and box shadow utilities

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,26 @@ const config: Config =  {
         './src/**/*.{astro,html,js,jsx,ts,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                'text-dark': '0px 4px 8px rgba(31, 31, 31, 0.8)',
+                'text-light': '0px 4px 8px rgba(243, 244, 246, 0.8)',
+            }
+        },
     },
     darkMode: 'class', // Enable dark mode support
     plugins: [
-        require('tailwind-scrollbar-hide')
+        require('tailwind-scrollbar-hide'),
+        function({ addUtilities }: Config ) {
+            addUtilities({
+                '.text-shadow-dark': {
+                    textShadow: '0px 4px 8px rgba(31, 31, 31, 0.6)',
+                },
+                '.text-shadow-light': {
+                    textShadow: '0px 4px 8px rgba(243, 244, 246, 0.3)',
+                },
+            })
+        }
     ],
 }
 

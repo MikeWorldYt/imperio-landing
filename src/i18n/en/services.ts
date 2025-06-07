cd040ba1c0478ef9bd5ef1deb8c82a2b958d2b28
mikeworldyt <diazmig98@gmail.com>
2025-06-06 19:06:50 -0600

    chore(i18n): reformat Ts

--- src/i18n/en/services.ts
+++ src/i18n/en/services.ts
@@ -1,4 +1,29 @@
 export const servicesTexts = {
-    title: 'Services - Imperio Pools',
-    subtitle: 'This is our services.'
+  title: 'Services - Imperio Pools',
+  subtitle: 'This is our services.',
+  services: [
+    {
+      id: 'pool-remodeling',
+      title: 'Pool Remodeling',
+      short: 'Transform your existing pool with our expert remodeling services...',
+      description: 'Transform your existing pool with our expert remodeling services. We enhance aesthetics and functionality, ensuring your pool meets modern standards and your personal style.',
+      features: [
+        'Custom designs tailored to your preferences',
+        'High-quality materials for durability',
+        'Energy-efficient upgrades available',
+        'Expert craftsmanship and attention to detail'
+      ]
+    },
+    {
+      id: 'replastering',
+      title: 'Replastering',
+      short: "",
+      description: "En - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      features: [
+        'Smooth, durable finishes',
+        'Wide range of plaster options',
+        'Improved longevity and water quality'
+      ]
+    }
+  ]
 };
--- src/i18n/es/services.ts
+++ src/i18n/es/services.ts
@@ -1,4 +1,29 @@
 export const servicesTexts = {
-    title: 'Servicios - Imperio Pools',
-    subtitle: 'Estos son nuestros servicios.'
+  title: 'Servicios - Imperio Pools',
+  subtitle: 'Estos son nuestros servicios.',
+  services: [
+    {
+      id: 'pool-remodeling',
+      title: 'Remodelelación de Piscinas',
+      short: "Transforma tu piscina existente con nuestros servicios de remodelación expertos...",
+      description: "Transforma tu piscina existente con nuestros servicios de remodelación expertos. Mejoramos la estética y funcionalidad, asegurando que tu piscina cumpla con los estándares modernos y tu estilo personal.",
+      features: [
+        'Custom designs tailored to your preferences',
+        'High-quality materials for durability',
+        'Energy-efficient upgrades available',
+        'Expert craftsmanship and attention to detail'
+      ]
+    },
+    {
+      id: 'replastering',
+      title: 'Replaster',
+      short: 'Revitalize ... desc desc',
+      description: "Es Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      features: [
+        'Smooth, durable finishes',
+        'Wide range of plaster options',
+        'Improved longevity and water quality'
+      ]
+    }
+  ]
 };

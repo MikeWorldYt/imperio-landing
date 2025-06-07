71ae935ea94de4fcae8a1b35fde40d55ca1e36cb
mikeworldyt <diazmig98@gmail.com>
2025-06-06 20:11:49 -0600

    feat(services): add short descriptions to services and update URLs for improved clarity

--- src/data/servicesData.ts
+++ src/data/servicesData.ts
@@ -1,9 +1,9 @@
 export interface Service {
     title: string;
+    short: string;
     description: string;
     image: string;
     icon: string;
-    subheading: string;
     btnPrimary: {
         href: string;
         className: string;
@@ -16,12 +16,13 @@ export interface Service {
     }[];
 }
 
-export const servicesAPI: Service[] = [{
-        title: 'Pool Remodeling',
+export const servicesAPI: Service[] = [
+    {
+        title: '<data>Pool Remodeling',
+        short: '<data>Modernize and enhance your existing pool or spa with expert ...',
         description: 'Modernize and enhance your existing pool or spa with expert renovations.',
         image: 'https://leisurepools.eu/wp-content/uploads/2020/06/best-type-of-swimming-pool-for-my-home_2.jpg',
         icon: '/icons/pool.svg',
-        subheading: 'In-Ground Pools',
         btnPrimary: [
             {
                 href :"get-quote/form?s=renovation",
@@ -31,7 +32,7 @@ export const servicesAPI: Service[] = [{
         ],
         btnSecondary: [
             {
-                href: "services",
+                href: "services/renovation",
                 className: "inline-block px-4 py-1 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 transition",
                 text: "Learn More"
             }
@@ -39,10 +40,11 @@ export const servicesAPI: Service[] = [{
     },
     {
         title: 'Replastering',
+        short: 'Transform your pool or spa with expert replastering services...',
         description: 'Transform your pool or spa with expert replastering services.',
         image: 'https://www.thefibreglasspoolcompany.com.au/wp-content/uploads/hamilton-sl-hero-1.jpg',
         icon: '/icons/spa.svg',
-        subheading: 'Wellness Upgrade',
+        
         btnPrimary: [
             {
                 href:"get-quote/form?s=plaster",
@@ -52,7 +54,7 @@ export const servicesAPI: Service[] = [{
         ],
         btnSecondary: [
             {
-                href:"services",
+                href:"services/replastering",
                 className:"inline-block px-4 py-1 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 transition",
                 text:"Learn More",
             }
@@ -60,10 +62,11 @@ export const servicesAPI: Service[] = [{
     },
     {
         title: 'Waterfalls & Fountains',
+        short: 'Enhance your pool or spa with stunning waterfalls and fountains...',
         description: 'Enhance your pool or spa with stunning waterfalls and fountains.',
         image: 'https://leisurepools.com.au/wp-content/uploads/2020/10/LeisurePoolsAU_Absolute_8m_SilverGrey_Rockhampton_2018-1.jpg',
         icon: '/icons/renovation.svg',
-        subheading: 'Pool Remodeling',
+        
         btnPrimary: [
             {
                 href:"get-quote/form?s=deco-features",
@@ -73,7 +76,7 @@ export const servicesAPI: Service[] = [{
         ],
         btnSecondary: [
             {
-                href:"services",
+                href:"services/deco-features",
                 className:"inline-block px-4 py-1 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 transition",
                 text:"Learn More",
             }
@@ -82,9 +85,9 @@ export const servicesAPI: Service[] = [{
     {
         title: 'Pool Deck',
         description: 'dec.',
+        short: 'Transform your pool area with our expert deck renovation services...',
         image: 'https://images.squarespace-cdn.com/content/v1/65eb3d8c9719340a9a48d8d7/6c62ce0a-e2cb-46b7-8ad8-b9df9e1db52f/Luxury+Custom+Pool+Seattle+-+Krisco+Pools+%282%29.jpg?format=2500w',
         icon: '/icons/renovation.svg',
-        subheading: 'Pool Remodeling',
         btnPrimary: [
             {
                 href:"get-quote/form?s=deck-bars",
@@ -103,9 +106,9 @@ export const servicesAPI: Service[] = [{
     {
         title: 'Test service 5',
         description: 'desc.',
+        short: 'This is a test service for demonstration purposes...',
         image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
         icon: '/icons/renovation.svg',
-        subheading: 'Pool Remodeling',
         btnPrimary: [
             {
                 href:"get-quote",

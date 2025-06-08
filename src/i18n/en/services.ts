946718cbe34036d4fdd0c748e88f1fe70144af31
mikeworldyt <diazmig98@gmail.com>
2025-06-07 20:13:22 -0600

    feat(i18n): add content field to Service interface

--- src/i18n/en/services.ts
+++ src/i18n/en/services.ts
@@ -8,6 +8,7 @@ export interface Service {
     description: string;
     image: string; // Only use 3:2 aspect ratio images
     icon: string;
+    content: string;
     features: string[];
     // btnSecondary: {
     //   href: string;
@@ -28,6 +29,7 @@ export const servicesTexts: Service = {
       description: 'Transform your existing pool with our expert remodeling services. We enhance aesthetics and functionality, ensuring your pool meets modern standards and your personal style.',
       image: "https://leisurepools.eu/wp-content/uploads/2020/06/best-type-of-swimming-pool-for-my-home_2.jpg",
       icon: '/icons/pool.svg',
+      content: "This is a <b>text</b> for demonstration purposes,<br> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
       features: [
         'Custom designs tailored to your preferences',
         'High-quality materials for durability',
@@ -42,6 +44,7 @@ export const servicesTexts: Service = {
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       image: "https://www.thefibreglasspoolcompany.com.au/wp-content/uploads/hamilton-sl-hero-1.jpg",
       icon: '/icons/spa.svg',
+      content: "<i>This is a <b>text</b> for demonstration purposes</i>, Lorem ipsum dolor<br> sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
       features: [
         'Smooth, durable finishes',
         'Wide range of plaster options',
@@ -55,6 +58,7 @@ export const servicesTexts: Service = {
       description: 'Enhance your pool or spa with stunning waterfalls and fountains, offering a unique and captivating visual experience. From cascading waterfalls to cascading fountains, our expertly crafted waterfalls and fountains add a touch of elegance and sophistication to your outdoor oasis.',
       image: 'https://leisurepools.com.au/wp-content/uploads/2020/10/LeisurePoolsAU_Absolute_8m_SilverGrey_Rockhampton_2018-1.jpg',
       icon: '/icons/renovation.svg',
+      content: "This is a <b>text for demonstration purposes</b>, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
       features: [
         'Beautiful waterfalls and fountains',
         'Custom designs and styles',
@@ -68,6 +72,7 @@ export const servicesTexts: Service = {
       description: "This is a test service for demonstration purposes.",
       image: 'https://images.squarespace-cdn.com/content/v1/65eb3d8c9719340a9a48d8d7/6c62ce0a-e2cb-46b7-8ad8-b9df9e1db52f/Luxury+Custom+Pool+Seattle+-+Krisco+Pools+%282%29.jpg?format=2500w',
       icon: '/icons/renovation.svg',
+      content: "This is a text for <b>demonstration</b> purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid.<br> Non voluptas esse amet voluptate! ",
       features: [
         'Beautiful waterfalls and fountains',
         'Custom designs and styles',
@@ -81,6 +86,7 @@ export const servicesTexts: Service = {
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
+      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
       features: [
         'Beautiful waterfalls and fountains',
         'Custom designs and styles',
--- src/i18n/es/services.ts
+++ src/i18n/es/services.ts
@@ -8,6 +8,7 @@ export interface Service {
     description: string;
     image: string; // Only use 3:2 aspect ratio images
     icon: string;
+    content: string;
     features: string[];
     // btnSecondary: {
     //   href: string;
@@ -28,6 +29,7 @@ export const servicesTexts: Service = {
       description: "Transforma tu piscina existente con nuestros servicios de remodelación expertos. Mejoramos la estética y funcionalidad, asegurando que tu piscina cumpla con los estándares modernos y tu estilo personal.",
       image: "https://leisurepools.eu/wp-content/uploads/2020/06/best-type-of-swimming-pool-for-my-home_2.jpg",
       icon: '/icons/pool.svg',
+      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
       features: [
         'Diseños personalizados y modernos',
         'Instalación segura y eficiente',
@@ -42,6 +44,7 @@ export const servicesTexts: Service = {
       description: "Es Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       image: "https://www.thefibreglasspoolcompany.com.au/wp-content/uploads/hamilton-sl-hero-1.jpg",
       icon: '/icons/spa.svg',
+      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
       features: [
         'Smooth, durable finishes',
         'Wide range of plaster options',
@@ -55,6 +58,7 @@ export const servicesTexts: Service = {
       description: "Lorem ipsum",
       image: "https://leisurepools.com.au/wp-content/uploads/2020/10/LeisurePoolsAU_Absolute_8m_SilverGrey_Rockhampton_2018-1.jpg",
       icon: '/icons/renovation.svg',
+      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
       features: [
         'editar',
         'editar',
@@ -69,6 +73,7 @@ export const servicesTexts: Service = {
       description: "Lorem ipsum",
       image: "https://images.squarespace-cdn.com/content/v1/65eb3d8c9719340a9a48d8d7/6c62ce0a-e2cb-46b7-8ad8-b9df9e1db52f/Luxury+Custom+Pool+Seattle+-+Krisco+Pools+%282%29.jpg?format=2500w",
       icon: '/icons/renovation.svg',
+      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
       features: [
         'editar',
         'editar',

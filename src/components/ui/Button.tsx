519dff952ee4de3e65fb7f402eb0a13d82138cf3
mikeworldyt <diazmig98@gmail.com>
2025-06-09 13:44:54 -0600

    style(Button): add new button variants for improved styling options

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,20 +2,18 @@ interface ButtonProps {
   text: string;
   href?: string;
   onClick?: () => void;
-  variant?: "primary_sm" | "secondary_sm" | "primary_lg" | "secondary_lg";
+  variant?: "primary_sm" | "secondary_sm" | "primary_lg" | "secondary_lg" | "mutted_sm" | "mutted_lg";
+  classAditional?: string;
 }
 
-export const Button = ({
-  text,
-  href,
-  onClick,
-  variant = "primary_sm",
-}: ButtonProps) => {
+export const Button:React.FC<ButtonProps> = ({text, href, onClick, variant = "primary_sm", classAditional}) => {
   const variantClass = {
     primary_sm: "btn-primary px-4 py-1",
     secondary_sm: "btn-secondary px-4 py-1",
+    mutted_sm: "btn-mutted px-4 py-1",
     primary_lg: "btn-primary px-4 py-2",
     secondary_lg: "btn-secondary px-4 py-2",
+    mutted_lg: "btn-mutted px-4 py-2",
   };
 
   const className = variantClass[variant];
@@ -24,7 +22,7 @@ export const Button = ({
     return (
       <a
         href={href}
-        className={className}
+        className={`${variantClass[variant]} ${classAditional ?? ""}`}
         target={href.startsWith("http") ? "_blank" : undefined}
         rel={href.startsWith("http") ? "noopener noreferrer" : undefined}
       >

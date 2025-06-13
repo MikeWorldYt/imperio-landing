534400a286c10ac45a89ea1e17690e51a2a543e5
mikeworldyt <diazmig98@gmail.com>
2025-06-13 00:10:03 -0600

    chore(i18n): add services and prepare forfill-in

--- src/i18n/en/services.ts
+++ src/i18n/en/services.ts
@@ -9,7 +9,7 @@ export interface Service {
     image: string; // Only use 3:2 aspect ratio images
     icon: string;
     content: string;
-    features: string[];
+    // features: string[];
     // btnSecondary: {
     //   href: string;
     //   className: string;
@@ -23,33 +23,31 @@ export const servicesTexts: Service = {
   subtitle: 'These are our services',
   services: [
     {
-      id: 'pool-remodeling',
+      id: 'pool-remodeling', // Renovations, Remodel, no Repairs
       title: 'Pool Remodeling',
       short: 'Transform your existing pool with our expert remodeling services...',
       description: 'Transform your existing pool with our expert remodeling services. We enhance aesthetics and functionality, ensuring your pool meets modern standards and your personal style.',
       image: "https://leisurepools.eu/wp-content/uploads/2020/06/best-type-of-swimming-pool-for-my-home_2.jpg",
       icon: '/icons/pool.svg',
       content: "This is a <b>text</b> for demonstration purposes,<br> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
-      features: [
-        'Custom designs tailored to your preferences',
-        'High-quality materials for durability',
-        'Energy-efficient upgrades available',
-        'Expert craftsmanship and attention to detail'
-      ]
     },
     {
-      id: 'replastering',
-      title: 'Replastering',
+      id: 'plastering', // 
+      title: 'Plastering',
       short: "Transform your pool or spa with expert replastering services...",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: "https://www.thefibreglasspoolcompany.com.au/wp-content/uploads/hamilton-sl-hero-1.jpg",
+      image: "https://images.squarespace-cdn.com/content/v1/65eb3d8c9719340a9a48d8d7/6c62ce0a-e2cb-46b7-8ad8-b9df9e1db52f/Luxury+Custom+Pool+Seattle+-+Krisco+Pools+%282%29.jpg?format=2500w",
       icon: '/icons/spa.svg',
-      content: "<i>This is a <b>text</b> for demonstration purposes</i>, Lorem ipsum dolor<br> sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
-      features: [
-        'Smooth, durable finishes',
-        'Wide range of plaster options',
-        'Improved longevity and water quality'
-      ]
+      content: "<i>This is a <b>text</b> for demonstration purposes</i> ",
+    },
+    {
+      id: 'addition', // Pool Construction, Planning, 
+      title: 'New Pools & Additionals',
+      short: 'This is a test service for demonstration purposes...',
+      description: "This is a test service for demonstration purposes",
+      image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
+      icon: '/icons/renovation.svg',
+      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
     },
     {
       id: 'waterfalls-and-fountains',
@@ -59,39 +57,33 @@ export const servicesTexts: Service = {
       image: 'https://leisurepools.com.au/wp-content/uploads/2020/10/LeisurePoolsAU_Absolute_8m_SilverGrey_Rockhampton_2018-1.jpg',
       icon: '/icons/renovation.svg',
       content: "This is a <b>text for demonstration purposes</b>, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
-      features: [
-        'Beautiful waterfalls and fountains',
-        'Custom designs and styles',
-        'Enhanced aesthetics and functionality'
-      ]
-    },
-    {   
-      id: 'pool-deck',
-      title: 'Pool Deck',
-      short: 'This is a test service for demonstration purposes...',
-      description: "This is a test service for demonstration purposes.",
-      image: 'https://images.squarespace-cdn.com/content/v1/65eb3d8c9719340a9a48d8d7/6c62ce0a-e2cb-46b7-8ad8-b9df9e1db52f/Luxury+Custom+Pool+Seattle+-+Krisco+Pools+%282%29.jpg?format=2500w',
-      icon: '/icons/renovation.svg',
-      content: "This is a text for <b>demonstration</b> purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid.<br> Non voluptas esse amet voluptate! ",
-      features: [
-        'Beautiful waterfalls and fountains',
-        'Custom designs and styles',
-        'Enhanced aesthetics and functionality'
-      ]
     },
     {
-      id: 'test-service-5',
-      title: 'Test service 5',
+      id: 'pool-equipment',
+      title: 'Pool Equipment Upgrades',
       short: 'This is a test service for demonstration purposes...',
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
       content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
-      features: [
-        'Beautiful waterfalls and fountains',
-        'Custom designs and styles',
-        'Enhanced aesthetics and functionality'
-      ]
+    },
+    {
+      id: "landscapes",
+      title: "Outdoor Kitchen",
+      short: 'This is a test service for demonstration purposes...',
+      description: "This is a test service for demonstration purposes",
+      image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
+      icon: '/icons/renovation.svg',
+      content: "This is a <b>text</b> for demonstration purposes",
+    },
+        {
+      id: "maintenence",
+      title: "Weekly Pool Service",
+      short: 'This is a test service for demonstration purposes...',
+      description: "This is a test service for demonstration purposes",
+      image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
+      icon: '/icons/renovation.svg',
+      content: "This is a <b>text</b> for demonstration purposes",
     },
   ]
 };
--- src/i18n/es/services.ts
+++ src/i18n/es/services.ts
@@ -9,7 +9,6 @@ export interface Service {
     image: string; // Only use 3:2 aspect ratio images
     icon: string;
     content: string;
-    features: string[];
     // btnSecondary: {
     //   href: string;
     //   className: string;
@@ -30,26 +29,15 @@ export const servicesTexts: Service = {
       image: "https://leisurepools.eu/wp-content/uploads/2020/06/best-type-of-swimming-pool-for-my-home_2.jpg",
       icon: '/icons/pool.svg',
       content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
-      features: [
-        'Diseños personalizados y modernos',
-        'Instalación segura y eficiente',
-        'Mantenimiento preventivo y correctivo',
-        'Garantía de calidad y durabilidad'
-      ]
     },
     {
-      id: 'replastering',
+      id: 'plastering',
       title: 'Replaster',
       short: 'Revitalize ... desc desc',
       description: "Es Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       image: "https://www.thefibreglasspoolcompany.com.au/wp-content/uploads/hamilton-sl-hero-1.jpg",
       icon: '/icons/spa.svg',
       content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
-      features: [
-        'Smooth, durable finishes',
-        'Wide range of plaster options',
-        'Improved longevity and water quality'
-      ]
     },
     {
       id: 'waterfalls-and-fountains',
@@ -59,28 +47,7 @@ export const servicesTexts: Service = {
       image: "https://leisurepools.com.au/wp-content/uploads/2020/10/LeisurePoolsAU_Absolute_8m_SilverGrey_Rockhampton_2018-1.jpg",
       icon: '/icons/renovation.svg',
       content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
-      features: [
-        'editar',
-        'editar',
-        'editar',
-        'editar'
-      ]
     },
-    {
-      id: 'pool-deck',
-      title: 'Instalación de Azulejos',
-      short: 'Revi... desc desc',
-      description: "Lorem ipsum",
-      image: "https://images.squarespace-cdn.com/content/v1/65eb3d8c9719340a9a48d8d7/6c62ce0a-e2cb-46b7-8ad8-b9df9e1db52f/Luxury+Custom+Pool+Seattle+-+Krisco+Pools+%282%29.jpg?format=2500w",
-      icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
-      features: [
-        'editar',
-        'editar',
-        'editar',
-        'editar'
-      ]
-    }
 
 
 

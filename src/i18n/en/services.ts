17a3410e4bdd23377c566a2b935ec736089bbf4a
mikeworldyt <diazmig98@gmail.com>
2025-06-14 19:14:17 -0600

    chore(i18n): support for dyanmic content blocks

--- src/i18n/en/services.ts
+++ src/i18n/en/services.ts
@@ -4,11 +4,12 @@ export interface Service {
   services: {
     id: string;
     title: string;
-    short: string;
     description: string;
     image: string; // Only use 3:2 aspect ratio images
     icon: string;
-    content: string;
+    content_1: string;
+    content_2?: string;
+    content_3?: string;
     // features: string[];
     // btnSecondary: {
     //   href: string;
@@ -25,65 +26,78 @@ export const servicesTexts: Service = {
     {
       id: 'pool-remodeling', // Renovations, Remodel, no Repairs
       title: 'Pool Remodeling',
-      short: 'Transform your existing pool with our expert remodeling services...',
-      description: 'Transform your existing pool with our expert remodeling services. We enhance aesthetics and functionality, ensuring your pool meets modern standards and your personal style.',
+      description: "Let's transform it from a simple pool into the stunning backyard oasis you’ve always dreamed of, making it the favorite spot for your family and friends.",
       image: "https://leisurepools.eu/wp-content/uploads/2020/06/best-type-of-swimming-pool-for-my-home_2.jpg",
       icon: '/icons/pool.svg',
-      content: "This is a <b>text</b> for demonstration purposes,<br> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      content_1: `
+
+      `,
+      content_2: `
+
+      `,
+      content_3: `
+
+      `,
     },
     {
       id: 'plastering', // 
       title: 'Plastering',
-      short: "Transform your pool or spa with expert replastering services...",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       image: "https://images.squarespace-cdn.com/content/v1/65eb3d8c9719340a9a48d8d7/6c62ce0a-e2cb-46b7-8ad8-b9df9e1db52f/Luxury+Custom+Pool+Seattle+-+Krisco+Pools+%282%29.jpg?format=2500w",
       icon: '/icons/spa.svg',
-      content: "<i>This is a <b>text</b> for demonstration purposes</i> ",
+      content_1: `
+        <i>This is a <b>text</b> for demonstration purposes</i> 
+      `,
     },
     {
       id: 'new-pools-and-additionals', // Pool Construction, Planning, 
       title: 'New Pools & Additionals',
-      short: 'This is a test service for demonstration purposes...',
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate!
+      `,
     },
     {
       id: 'waterfalls-and-fountains',
       title: 'Waterfalls & Fountains',
-      short: 'Enhance your pool or spa with stunning waterfalls and fountains...',
       description: 'Enhance your pool or spa with stunning waterfalls and fountains, offering a unique and captivating visual experience. From cascading waterfalls to cascading fountains, our expertly crafted waterfalls and fountains add a touch of elegance and sophistication to your outdoor oasis.',
       image: 'https://leisurepools.com.au/wp-content/uploads/2020/10/LeisurePoolsAU_Absolute_8m_SilverGrey_Rockhampton_2018-1.jpg',
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text for demonstration purposes</b>, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      content_1: `
+        This is a <b>text for demonstration purposes</b>, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate!
+      `,
     },
     {
       id: 'pool-equipment-upgrades',
       title: 'Pool Equipment Upgrades',
-      short: 'This is a test service for demonstration purposes...',
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! 
+      `,
     },
     {
       id: "outdoor-living", // Landscapes
       title: "Outdoor Living",
-      short: 'This is a test service for demonstration purposes...',
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes
+      `,
     },
         {
       id: "weekly-pool-services",
       title: "Weekly Pool Services",
-      short: 'This is a test service for demonstration purposes...',
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes
+      `,
     },
   ]
 };
--- src/i18n/es/services.ts
+++ src/i18n/es/services.ts
@@ -4,11 +4,12 @@ export interface Service {
   services: {
     id: string;
     title: string;
-    short: string;
     description: string;
     image: string; // Only use 3:2 aspect ratio images
     icon: string;
-    content: string;
+    content_1: string;
+    content_2?: string;
+    content_3?: string;
     // btnSecondary: {
     //   href: string;
     //   className: string;
@@ -20,69 +21,76 @@ export interface Service {
 export const servicesTexts: Service = {
   title: 'Servicios - Imperio Pools',
   subtitle: 'Estos son nuestros servicios.',
-  services: [
+  services: [ 
     {
       id: 'pool-remodeling',
       title: 'Remodelelación de Piscinas',
-      short: "Transforma tu piscina existente con nuestros servicios de remodelación expertos...",
       description: "Transforma tu piscina existente con nuestros servicios de remodelación expertos. Mejoramos la estética y funcionalidad, asegurando que tu piscina cumpla con los estándares modernos y tu estilo personal.",
       image: "https://leisurepools.eu/wp-content/uploads/2020/06/best-type-of-swimming-pool-for-my-home_2.jpg",
       icon: '/icons/pool.svg',
-      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      `
     },
     {
       id: 'plastering',
       title: 'Replaster',
-      short: 'Revitalize ... desc desc',
       description: "Es Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       image: "https://www.thefibreglasspoolcompany.com.au/wp-content/uploads/hamilton-sl-hero-1.jpg",
       icon: '/icons/spa.svg',
-      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes
+      `,
     },
     {
       id: 'new-pools-and-additionals', // Pool Construction, Planning, 
       title: 'Nuevas Piscinas y Ampliaciones',
-      short: 'This is a test service for demonstration purposes...',
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes
+      `,
     },
     {
       id: 'waterfalls-and-fountains',
       title: 'Cascadas y Fuentes',
-      short: 'Revitalize ... desc desc',
       description: "Lorem ipsum",
       image: "https://leisurepools.com.au/wp-content/uploads/2020/10/LeisurePoolsAU_Absolute_8m_SilverGrey_Rockhampton_2018-1.jpg",
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes
+      `,
     },
     {
       id: 'pool-equipment-upgrades',
       title: 'Equipos de Piscina',
-      short: 'This is a test service for demonstration purposes...',
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti non eius corrupti recusandae repellat, nulla commodi amet modi delectus nam consectetur in tenetur molestias aliquid. Non voluptas esse amet voluptate! ",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes
+      `,
     },
     {
       id: "outdoor-living", // Landscapes
       title: "Patios",
-      short: 'This is a test service for demonstration purposes...',
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes
+      `,
     },
         {
       id: "weekly-pool-services",
       title: "Servicios Semanales",
-      short: 'This is a test service for demonstration purposes...',
       description: "This is a test service for demonstration purposes",
       image: 'https://www.385swim.com/wp-content/uploads/2020/10/Rochester-NY-In-Ground-Pool-Installer-41-scaled.jpg',
       icon: '/icons/renovation.svg',
-      content: "This is a <b>text</b> for demonstration purposes",
+      content_1: `
+        This is a <b>text</b> for demonstration purposes
+      `,
     },
   ]
 };
